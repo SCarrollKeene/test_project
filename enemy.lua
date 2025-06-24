fe43015ba7d52d1833aeac1435d9553adb315f14
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2025-06-23 21:46:42 -0600

    cleaned up some debug code out of enemy.lua

--- enemy.lua
+++ enemy.lua
@@ -277,14 +277,6 @@ function Enemy:draw()
     end
         love.graphics.setColor(1, 1, 1, 1) -- color reset
         -- love.graphics.pop()
-        -- self.world:draw()
-
-    -- Show tracking line
-    -- if debugMode and self.target then
-    --     love.graphics.setColor(1, 0, 0)
-    --     love.graphics.line(self.x, self.y, self.target.x, self.target.y)
-    --     love.graphics.setColor(1, 1, 1)
-    -- end
 end
 
 -- take damage, deal damage and direction
